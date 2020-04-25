43f2bd5b380479b3290e2f0e247e1f4962e38afa
Stijn Van der Borght <stijnvanderborght@gmail.com>
2020-04-25 16:37:12 +0100

    set assembly version to 0.19.0.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -63,5 +63,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.19.0")]
-[assembly: AssemblyFileVersion("0.0.19.0")]
+[assembly: AssemblyVersion("0.19.0.3")]
+[assembly: AssemblyFileVersion("0.19.0.3")]
