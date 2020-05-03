af997129fb6f5a0ac4fb1ee0cb1285ec2b43bd1d
Stijn Van der Borght <stijnvanderborght@gmail.com>
2020-05-03 22:25:06 +0100

    added edit group page

--- Mobile/Mobile/Views/GroupEditPage.xaml.cs
+++ Mobile/Mobile/Views/GroupEditPage.xaml.cs
@@ -51,34 +51,65 @@ namespace SnapDotNet.Mobile.Views
 
         private void _UpdateData()
         {
-            //Title = m_Group.Name;
-            //tbName.Text = m_Group.name;
+            Title = m_Group.Name;
+            eName.Text = m_Group.name;
+            pStream.Items.Clear();
+            foreach (SnapDotNet.ControlClient.JsonRpcData.Stream stream in m_SnapcastClient.ServerData.streams)
+            {
+                pStream.Items.Add(stream.id);
+            }
 
-            //cbStream.Items.Clear();
-            //foreach (SnapDotNet.ControlClient.JsonRpcData.Stream stream in m_SnapcastClient.ServerData.streams)
-            //{
-            //    cbStream.Items.Add(stream.id);
-            //}
+            m_Clients = m_SnapcastClient.ServerData.GetAllClients();
+            slClients.Children.Clear();
+            m_ClientCheckBoxes = new CheckBox[m_Clients.Length];
+            for (int i = 0; i < m_ClientCheckBoxes.Length; i++)
+            {
+                StackLayout sl = new StackLayout();
+                sl.Orientation = StackOrientation.Horizontal;
+                
+                Label lb = new Label();
+                TapGestureRecognizer tr = new TapGestureRecognizer();
+                int idx = i;
+                tr.Tapped += (sender, e) =>
+                {
+                    m_ClientCheckBoxes[idx].IsChecked = m_ClientCheckBoxes[idx].IsChecked == false;
+                };
+                sl.GestureRecognizers.Add(tr);
+                lb.Text = m_Clients[i].Name;
+                lb.HorizontalOptions = LayoutOptions.StartAndExpand;
+                sl.Children.Add(lb);
 
-            //m_Clients = m_SnapcastClient.ServerData.GetAllClients();
-            //spClients.Children.Clear();
-            //m_ClientCheckBoxes = new CheckBox[m_Clients.Length];
-            //for (int i = 0; i < m_ClientCheckBoxes.Length; i++)
-            //{
-            //    CheckBox cb = new CheckBox();
-            //    cb.Content = m_Clients[i].Name;
-            //    //cb.FlowDirection = FlowDirection.RightToLeft; // (we want the text on the left side of the checkbox...)
-            //    //TextBlock tb = new TextBlock();
-            //    //tb.FlowDirection = FlowDirection.LeftToRight;
-            //    //tb.Text = m_Clients[i].Name;
-            //    //cb.Content = tb;
-            //    cb.Margin = new Thickness(10, 0, 0, 5);
-            //    cb.IsChecked = m_Group.HasClientWithId(m_Clients[i].id);
-            //    m_ClientCheckBoxes[i] = cb;
-            //    spClients.Children.Add(m_ClientCheckBoxes[i]);
-            //}
+                CheckBox cb = new CheckBox();
+                cb.Margin = new Thickness(10, 0, 0, 5);
+                cb.IsChecked = m_Group.HasClientWithId(m_Clients[i].id);
 
-            //cbStream.SelectedItem = m_Group.stream_id;
+                m_ClientCheckBoxes[i] = cb;
+                sl.Children.Add(cb);
+
+                slClients.Children.Add(sl);
+            }
+
+            pStream.SelectedItem = m_Group.stream_id;
+        }
+
+        private string[] _GetSelectedClientIds()
+        {
+            List<string> ids = new List<string>();
+            for (int i = 0; i < m_ClientCheckBoxes.Length; i++)
+            {
+                if (m_ClientCheckBoxes[i].IsChecked == true)
+                {
+                    ids.Add(m_Clients[i].id);
+                }
+            }
+            return ids.ToArray();
+        }
+
+        private void GroupEditPage_OnDisappearing(object sender, EventArgs e)
+        {
+            m_Group.CLIENT_SetName(eName.Text);
+            m_Group.CLIENT_SetStream(pStream.SelectedItem.ToString());
+            m_Group.CLIENT_SetClients(_GetSelectedClientIds());
         }
     }
 }
