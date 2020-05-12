1f642d475116fc6a3ba0b7f8b16049ef6df8a0d1
Stijn Van der Borght <stijnvanderborght@gmail.com>
2020-05-12 01:39:32 +0100

    publishing settings

--- Mobile/Mobile/AssemblyInfo.cs
+++ Mobile/Mobile/AssemblyInfo.cs
@@ -1,3 +1,10 @@
+using Android.App;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable = false)]
+#endif
