bdb465f046bdc168efe2e629d4d8c4d055823aeb
Stijn Van der Borght <stijnvanderborght@gmail.com>
2020-05-12 11:07:39 +0100

    fix compile error, update screenshots

--- Mobile/Mobile/AssemblyInfo.cs
+++ Mobile/Mobile/AssemblyInfo.cs
@@ -1,10 +1,3 @@
-using Android.App;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-
-#if DEBUG
-[assembly: Application(Debuggable=true)]
-#else
-[assembly: Application(Debuggable = false)]
-#endif
