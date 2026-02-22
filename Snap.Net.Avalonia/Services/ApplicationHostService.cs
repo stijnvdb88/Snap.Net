f7d33589ab659cfa88bb9b200a4a433f0608db37
Stijn Van der Borght <stijnvanderborght@gmail.com>
2026-02-22 12:08:56 +0000

    downgrade avalonia to 11.3.12 again, Xaml.Behaviors.Avalonia dependency doesn't support 12 yet - see https://github.com/wieslawsoltes/Xaml.Behaviors/issues/331

--- Snap.Net.Avalonia/Services/ApplicationHostService.cs
+++ Snap.Net.Avalonia/Services/ApplicationHostService.cs
@@ -66,8 +66,22 @@ public class ApplicationHostService : IHostedService
         {
             desktop.ShutdownMode = ShutdownMode.OnExplicitShutdown;
         }
-
+        // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
+        // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
+        DisableAvaloniaDataAnnotationValidation();
         return Task.CompletedTask;
     }
+    
+    private void DisableAvaloniaDataAnnotationValidation()
+    {
+        // Get an array of plugins to remove
+        DataAnnotationsValidationPlugin[] dataValidationPluginsToRemove =
+            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
 
+        // remove each entry found
+        foreach (DataAnnotationsValidationPlugin plugin in dataValidationPluginsToRemove)
+        {
+            BindingPlugins.DataValidators.Remove(plugin);
+        }
+    }
 }
