0973014eab8fc6641cf7b3e38a5dcde13d895aa2
Stijn Van der Borght <stijnvanderborght@gmail.com>
2026-05-02 21:45:33 +0100

    oops

--- Snap.Net.Avalonia/ViewModels/AppViewModel.cs
+++ Snap.Net.Avalonia/ViewModels/AppViewModel.cs
@@ -92,9 +92,3 @@ public partial class AppViewModel : ViewModelBase
         }
     }
 }
-
-
-        - name: Restore
-          run: |
-            dotnet restore Snap.Net.SnapClient/Snap.Net.SnapClient.csproj
-            dotnet restore Mobile/Mobile/Snap.Net.Mobile.csproj
